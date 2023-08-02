764b7c5cc1eb5dc6b82753d7a9d03ae8460cb60a
John Glen Falceso <johnglenfalceso@protonmail.com>
2023-08-02 13:33:06 +0800

    added education section and initial projects section

--- scripts/scramble.js
+++ scripts/scramble.js
@@ -1,76 +1,125 @@
+const coursework = document.querySelector('.coursework');
+let courseWorkCount = 0;
+
 class TextScramble {
-    constructor(el) {
-      this.el = el
-      this.chars = '!<>-_\\/[]{}—=+*^?#________'
-      this.update = this.update.bind(this)
+  constructor(el) {
+    this.el = el
+    this.chars = '!<>-_\\/[]{}—=+*^?#________'
+    this.update = this.update.bind(this)
+  }
+  setText(newText) {
+    const oldText = this.el.innerText
+    const length = Math.max(oldText.length, newText.length)
+    const promise = new Promise((resolve) => this.resolve = resolve)
+    this.queue = []
+    for (let i = 0; i < length; i++) {
+      const from = oldText[i] || ''
+      const to = newText[i] || ''
+      const start = Math.floor(Math.random() * 40)
+      const end = start + Math.floor(Math.random() * 40)
+      this.queue.push({ from, to, start, end })
     }
-    setText(newText) {
-      const oldText = this.el.innerText
-      const length = Math.max(oldText.length, newText.length)
-      const promise = new Promise((resolve) => this.resolve = resolve)
-      this.queue = []
-      for (let i = 0; i < length; i++) {
-        const from = oldText[i] || ''
-        const to = newText[i] || ''
-        const start = Math.floor(Math.random() * 40)
-        const end = start + Math.floor(Math.random() * 40)
-        this.queue.push({ from, to, start, end })
-      }
-      cancelAnimationFrame(this.frameRequest)
-      this.frame = 0
-      this.update()
-      return promise
-    }
-    update() {
-      let output = ''
-      let complete = 0
-      for (let i = 0, n = this.queue.length; i < n; i++) {
-        let { from, to, start, end, char } = this.queue[i]
-        if (this.frame >= end) {
-          complete++
-          output += to
-        } else if (this.frame >= start) {
-          if (!char || Math.random() < 0.28) {
-            char = this.randomChar()
-            this.queue[i].char = char
-          }
-          output += `<span class="dud">${char}</span>`
-        } else {
-          output += from
+    cancelAnimationFrame(this.frameRequest)
+    this.frame = 0
+    this.update()
+    return promise
+  }
+  update() {
+    let output = ''
+    let complete = 0
+    for (let i = 0, n = this.queue.length; i < n; i++) {
+      let { from, to, start, end, char } = this.queue[i]
+      if (this.frame >= end) {
+        complete++
+        output += to
+      } else if (this.frame >= start) {
+        if (!char || Math.random() < 0.28) {
+          char = this.randomChar()
+          this.queue[i].char = char
         }
-      }
-      this.el.innerHTML = output
-      if (complete === this.queue.length) {
-        this.resolve()
+        output += `<span class="dud">${char}</span>`
       } else {
-        this.frameRequest = requestAnimationFrame(this.update)
-        this.frame++
+        output += from
       }
     }
-    randomChar() {
-      return this.chars[Math.floor(Math.random() * this.chars.length)]
+    this.el.innerHTML = output
+    if (complete === this.queue.length) {
+      this.resolve()
+    } else {
+      this.frameRequest = requestAnimationFrame(this.update)
+      this.frame++
     }
   }
-  
-  // ——————————————————————————————————————————————————
-  // Example
-  // ——————————————————————————————————————————————————
-  
-  const phrases = [
-    'Computer Science Student',
-    'Aspiring Software Engineer',
-    'Tea-Powered Coder'
-  ]
-  
-  const el = document.querySelector('.about-name')
-  const fx = new TextScramble(el)
-  
-  let counter = 0
-  const next = () => {
-    fx.setText(phrases[counter]).then(() => {
-      setTimeout(next, 1500)
-    })
-    counter = (counter + 1) % phrases.length
+  randomChar() {
+    return this.chars[Math.floor(Math.random() * this.chars.length)]
   }
-  
-  next()
+}
+
+// if element is in view port
+function isInViewport(el) {
+  const rect = el.getBoundingClientRect();
+  return (
+    rect.top >= 0 &&
+    rect.left >= 0 &&
+    rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
+    rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+  );
+}
+
+// ——————————————————————————————————————————————————
+// Example
+// ——————————————————————————————————————————————————
+
+const phrases = [
+  'Computer Science Student',
+  'Aspiring Software Engineer',
+  'Tea-Powered Coder'
+]
+
+// name section scramble effect
+const el = document.querySelector('.about-name')
+const fx = new TextScramble(el)
+
+let counter = 0
+const next = () => {
+  fx.setText(phrases[counter]).then(() => {
+    setTimeout(next, 1500)
+  })
+  counter = (counter + 1) % phrases.length
+}
+
+next()
+
+
+// scroll scramble effect
+document.addEventListener('scroll', function () {
+  /* relative coursework */
+  if (courseWorkCount == 0 && isInViewport(coursework)) {
+    console.log('galawa')
+    const collegeCourseWorkPhrases = [
+      'Computer Programming',
+      'Object-Oriented Programming',
+      'Information Management',
+      'Data Structures and Algorithms',
+      'Design and Analysis of Algorithms',
+      'Software Development'
+    ]
+
+    let counter = 0
+    const fx = new TextScramble(coursework)
+    const next = () => {
+      fx.setText(collegeCourseWorkPhrases[counter]).then(() => {
+        setTimeout(next, 1500)
+      })
+      counter = (counter + 1) % collegeCourseWorkPhrases.length
+    }
+
+    next();
+
+    courseWorkCount = 1;
+  }
+
+}, {
+  passive: true
+});
+
---
+++ scripts/tech.js
@@ -0,0 +1,134 @@
+const cards = document.querySelector('.cards');
+
+const colorMap = {
+    'python': 'yellow',
+    'pandas': 'blue',
+    'scikit-learn': 'orange',
+    'streamlit': 'red',
+    'numpy': 'blue',
+    'javascript': 'yellow',
+    'html': 'orange',
+    'css': 'blue',
+    'next': 'purple',
+    'node': 'green',
+    'firebase': 'orange',
+    'java': 'orange',
+    'cli': 'green',
+    'njs': 'purple',
+    'fastapi': 'green',
+    'flask': 'green',
+    'kotlin': 'orange',
+    'android': 'green',
+    'realm-orm': 'purple'
+}
+
+const cardsInfo = [
+    {
+        title: 'Phoney',
+        description: 'Counterfeit mobile phone product classifier',
+        tech: ['python', 'pandas', 'scikit-learn', 'numpy', 'streamlit'],
+        srcURL: 'https://github.com/CodeJohnGlenx',
+        liveURL: 'https://htmlcolorcodes.com/'
+    },
+    {
+        title: 'Wonderpets',
+        description: 'Mobile web application for online pet adoption',
+        tech: ['html', 'css', 'javascript', 'next', 'node', 'firebase'],
+        srcURL: 'https://github.com/CodeJohnGlenx',
+        liveURL: ''
+    },
+    {
+        title: 'nJS',
+        description: 'Statically-typed JavaScript based from the principles of Java programming',
+        tech: ['njs', 'java', 'cli'],
+        srcURL: 'https://github.com/CodeJohnGlenx',
+        liveURL: ''
+    },
+    {
+        title: 'Currency Deno API',
+        description: 'Conversion between two currencies and the nearest denomination of the converted currency',
+        tech: ['python', 'fastapi'],
+        srcURL: 'https://github.com/CodeJohnGlenx',
+        liveURL: ''
+    },
+    {
+        title: 'Anagram-esque',
+        description: 'Seamless generation of english words based on user letter inputs',
+        tech: ['html', 'css', 'javascript', 'python', 'flask'],
+        srcURL: 'https://github.com/CodeJohnGlenx',
+        liveURL: 'https://htmlcolorcodes.com/'
+    },
+    {
+        title: 'SaveUp',
+        description: 'Native android application for expenditure and budget management',
+        tech: ['kotlin', 'realm-orm', 'android'],
+        srcURL: 'https://github.com/CodeJohnGlenx',
+        liveURL: ''
+    },
+    {
+        title: 'Origami',
+        description: 'Origami models tutorial website',
+        tech: ['html', 'css', 'javascript'],
+        srcURL: 'https://github.com/CodeJohnGlenx',
+        liveURL: 'https://htmlcolorcodes.com/'
+    },
+]
+
+/* create cards */
+for (const cardInfo of cardsInfo) {
+    const card = document.createElement('div');
+    card.setAttribute('class', 'card')
+
+    const cardBody = document.createElement('div');
+    cardBody.setAttribute('class', 'card-body');
+
+    const cardTitle = document.createElement('h4');
+    cardTitle.setAttribute('class', 'card-title');
+    cardTitle.textContent = cardInfo.title;
+
+    const cardDescription = document.createElement('p');
+    cardDescription.setAttribute('class', "card-text description");
+    cardDescription.textContent = cardInfo.description;
+
+    const technologies = document.createElement('div');
+    technologies.setAttribute('class', 'technologies');
+    for (const t of cardInfo.tech) {
+        const tech = document.createElement('span');
+        tech.setAttribute('class', 'tech');
+        tech.classList.add(colorMap[t]);
+        tech.textContent = t;
+        technologies.appendChild(tech);
+    }
+
+    const cardLinks = document.createElement('div');
+    cardLinks.setAttribute('class', 'card-links');
+
+    const srcCode = document.createElement('a');
+    srcCode.setAttribute('href', cardInfo.srcURL);
+    srcCode.setAttribute('target', '_blank');
+    const srcSpan = document.createElement('span');
+    srcSpan.setAttribute('class', 'card-text');
+    srcSpan.textContent = 'Source Code';
+    srcCode.appendChild(srcSpan);
+
+    cardLinks.appendChild(srcCode);
+
+    if (cardInfo.liveURL !== '') {
+        const liveURL = document.createElement('a');
+        liveURL.setAttribute('href', cardInfo.liveURL);
+        liveURL.setAttribute('target', '_blank');
+        const liveSpan = document.createElement('span');
+        liveSpan.setAttribute('class', 'card-text');
+        liveSpan.textContent = 'Live Demo';
+    
+        liveURL.appendChild(liveSpan);
+        cardLinks.appendChild(liveURL);
+    }
+
+    cardBody.appendChild(cardTitle);
+    cardBody.appendChild(cardDescription);
+    cardBody.appendChild(technologies);
+    cardBody.appendChild(cardLinks);
+    card.appendChild(cardBody);
+    cards.appendChild(card);
+}
